54e439a94c3a61636a1036b7d66880e692203539
Marsouin <thomas.louvet@epitech.eu>
2026-08-04 15:03:19 -0400

    Adding cmake to replace the dirty handed makefile -> it works

---
+++ build/CMakeFiles/Platine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Platine.dir/code/src/MapContentReader.cpp.o"
+  "CMakeFiles/Platine.dir/code/src/MapContentReader.cpp.o.d"
+  "CMakeFiles/Platine.dir/code/src/MapData.cpp.o"
+  "CMakeFiles/Platine.dir/code/src/MapData.cpp.o.d"
+  "CMakeFiles/Platine.dir/code/src/main.cpp.o"
+  "CMakeFiles/Platine.dir/code/src/main.cpp.o.d"
+  "Platine"
+  "Platine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Platine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Platine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Platine.
+# This may be replaced when dependencies are built.
